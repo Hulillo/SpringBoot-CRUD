c06584dc21765dcd23bff19fc9810d05f79f27c9
Anfeh <alexvictoria232@gmail.com>
2024-02-19 09:49:16 +0100

    feat: Malas practicas resueltas, POO anadido

---
+++ src/main/java/org/example/springboot_crud/Datos/Conexion.java
@@ -0,0 +1,58 @@
+package org.example.springboot_crud.Datos;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class Conexion {
+    private final String DATABASE_NAME = "usuarios";
+    private final String USER_MONGO_CONNECT = "mongodb://localhost:27017";
+    private final String COLLECTION_NAME = "usuarios";
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+
+    public Conexion() {
+        mongoClient = MongoClients.create(USER_MONGO_CONNECT);
+        database = mongoClient.getDatabase(DATABASE_NAME);
+        collection = database.getCollection(COLLECTION_NAME);
+    }
+
+    public String getDATABASE_NAME() {
+        return DATABASE_NAME;
+    }
+
+    public String getUSER_MONGO_CONNECT() {
+        return USER_MONGO_CONNECT;
+    }
+
+    public String getCOLLECTION_NAME() {
+        return COLLECTION_NAME;
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public void setMongoClient(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(MongoDatabase database) {
+        this.database = database;
+    }
+
+    public MongoCollection<Document> getCollection() {
+        return collection;
+    }
+
+    public void setCollection(MongoCollection<Document> collection) {
+        this.collection = collection;
+    }
+}
--- src/main/java/org/example/springboot_crud/controlador/FormsController.java
+++ src/main/java/org/example/springboot_crud/controlador/FormsController.java
@@ -7,6 +7,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.example.springboot_crud.Datos.Conexion;
 import org.example.springboot_crud.Datos.Libro;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,48 +27,45 @@ public class FormsController {
     }
     @PostMapping("/submit-formulario")
     public String procesarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
-        final String DATABASE_NAME = "usuarios";
-        final String USER_MONGO_CONNECT = "mongodb://localhost:27017";
-        final String COLLECTION_NAME = "usuarios";
-        MongoClient mongoClient;
-        MongoDatabase database;
-        MongoCollection<Document> collection;
+        Conexion conexion = new Conexion();
         Document filtro, usuario;
-        mongoClient = MongoClients.create(USER_MONGO_CONNECT);
-        database = mongoClient.getDatabase(DATABASE_NAME);
-        collection = database.getCollection(COLLECTION_NAME);
         String datos = null;
         String usuarioMongo, contrasenaMongo, emailMongo;
-        List<Libro> libros = new ArrayList<>(); // Lista para almacenar los libros
+        List<Libro> libros = new ArrayList<>();
+
 
 
         try {
             filtro = new Document("usuario", nombre);
-            usuario = collection.find(filtro).first();
+            usuario = conexion.getCollection().find(filtro).first();
             JsonParser jsonParser = new JsonParser();
+            JsonObject libroObj;
+            String titulo,autor,sinopsis;
             JsonObject jsonObject = (JsonObject) jsonParser.parse(usuario.toJson());
-
-
             usuarioMongo = jsonObject.get("usuario").getAsString();
             contrasenaMongo = jsonObject.get("password").getAsString();
             emailMongo = jsonObject.get("email").getAsString();
-
             JsonArray librosJson = jsonObject.getAsJsonArray("libros");
+
+
+
             for (JsonElement libroJson : librosJson) {
-                JsonObject libroObj = libroJson.getAsJsonObject();
-                String titulo = libroObj.get("titulo").getAsString();
-                String autor = libroObj.get("autor").getAsString();
-                String sinopsis = libroObj.get("sinopsis").getAsString();
+                libroObj = libroJson.getAsJsonObject();
+                titulo = libroObj.get("titulo").getAsString();
+                autor = libroObj.get("autor").getAsString();
+                sinopsis = libroObj.get("sinopsis").getAsString();
                 libros.add(new Libro(titulo,autor,sinopsis));
             }
-
             if(usuarioMongo.equals(nombre) && contrasenaMongo.equals(password)){
                 model.addAttribute("nombre", usuarioMongo);
                 model.addAttribute("password", contrasenaMongo);
                 model.addAttribute("email", emailMongo);
-                model.addAttribute("libros", libros); // Pasar la lista de libros al modelo
+                model.addAttribute("libros", libros);
                 datos = "usuario";
             }
+
+
+
         } catch (MongoException e) {
             System.err.println("Error al obtener usuario: " + e.getMessage());
         } catch (NullPointerException e) {
