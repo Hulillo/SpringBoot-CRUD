6eef46eadc5504dceedca290b7ce100435f43450
Anfeh <alexvictoria232@gmail.com>
2024-02-27 13:36:51 +0100

    fix: DELETE lineas codigo no utiles

--- src/main/java/org/example/springboot_crud/controlador/FormsController.java
+++ src/main/java/org/example/springboot_crud/controlador/FormsController.java
@@ -34,52 +34,7 @@ public class FormsController {
 
         return "insertar";
     }
-
-    @PostMapping("/submit-formulario-actualizado")
-    public String mostrarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
-        Conexion conexion = new Conexion();
-        Document filtro, usuario;
-        String datos = null;
-        String usuarioMongo, contrasenaMongo, emailMongo;
-        List<Libro> libros = new ArrayList<>();
-
-
-        try {
-            filtro = new Document("usuario", nombre);
-            usuario = conexion.getCollection().find(filtro).first();
-            JsonParser jsonParser = new JsonParser();
-            JsonObject libroObj;
-            String titulo,autor,sinopsis;
-            JsonObject jsonObject = (JsonObject) jsonParser.parse(usuario.toJson());
-            usuarioMongo = jsonObject.get("usuario").getAsString();
-            contrasenaMongo = jsonObject.get("password").getAsString();
-            emailMongo = jsonObject.get("email").getAsString();
-            JsonArray librosJson = jsonObject.getAsJsonArray("libros");
-
-            for (JsonElement libroJson : librosJson) {
-                libroObj = libroJson.getAsJsonObject();
-                titulo = libroObj.get("titulo").getAsString();
-                autor = libroObj.get("autor").getAsString();
-                sinopsis = libroObj.get("sinopsis").getAsString();
-                libros.add(new Libro(titulo,autor,sinopsis));
-            }
-            if(usuarioMongo.equals(nombre) && contrasenaMongo.equals(password)){
-                model.addAttribute("nombre", usuarioMongo);
-                model.addAttribute("password", contrasenaMongo);
-                model.addAttribute("email", emailMongo);
-                model.addAttribute("libros", libros);
-                datos = "usuario";
-            }
-
-
-
-        } catch (MongoException e) {
-            System.err.println("Error al obtener usuario: " + e.getMessage());
-        } catch (NullPointerException e) {
-            System.err.println("No existe dicho usuario.");
-        }
-        return datos;
-    }
+    
 
     @PostMapping("/submit-formulario")
     public String procesarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
