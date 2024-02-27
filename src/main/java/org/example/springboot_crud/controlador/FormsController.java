8ae693be804583d61494f592aae3816e37bcd6b8
Julio <juliomonterobenitez@gmail.com>
2024-02-27 09:51:06 +0100

    feat: insertar desde otra pestaña realizado

--- src/main/java/org/example/springboot_crud/controlador/FormsController.java
+++ src/main/java/org/example/springboot_crud/controlador/FormsController.java
@@ -1,14 +1,14 @@
 package org.example.springboot_crud.controlador;
 
-import com.google.gson.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mongodb.MongoException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.example.springboot_crud.Datos.Conexion;
 import org.example.springboot_crud.Datos.Libro;
+import org.example.springboot_crud.Datos.Operaciones;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,14 +25,23 @@ public class FormsController {
     public String mostrarLogin() {
         return "login";
     }
-    @PostMapping("/submit-formulario")
-    public String procesarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
+
+    @PostMapping("/insertar")
+    public String mostrarInsertar(@RequestParam String nombre, @RequestParam String password, Model model) {
+
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("password", password);
+
+        return "insertar";
+    }
+
+    @PostMapping("/submit-formulario-actualizado")
+    public String mostrarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
         Conexion conexion = new Conexion();
         Document filtro, usuario;
         String datos = null;
         String usuarioMongo, contrasenaMongo, emailMongo;
         List<Libro> libros = new ArrayList<>();
-
 
 
         try {
@@ -46,8 +55,6 @@ public class FormsController {
             contrasenaMongo = jsonObject.get("password").getAsString();
             emailMongo = jsonObject.get("email").getAsString();
             JsonArray librosJson = jsonObject.getAsJsonArray("libros");
-
-
 
             for (JsonElement libroJson : librosJson) {
                 libroObj = libroJson.getAsJsonObject();
@@ -72,6 +79,70 @@ public class FormsController {
             System.err.println("No existe dicho usuario.");
         }
         return datos;
+    }
+
+    @PostMapping("/submit-formulario")
+    public String procesarFormulario(@RequestParam String nombre, @RequestParam String password, Model model) {
+        Conexion conexion = new Conexion();
+        Document filtro, usuario;
+        String datos = null;
+        String usuarioMongo, contrasenaMongo, emailMongo;
+        List<Libro> libros = new ArrayList<>();
+
+
+        try {
+            filtro = new Document("usuario", nombre);
+            usuario = conexion.getCollection().find(filtro).first();
+            JsonParser jsonParser = new JsonParser();
+            JsonObject libroObj;
+            String titulo,autor,sinopsis;
+            JsonObject jsonObject = (JsonObject) jsonParser.parse(usuario.toJson());
+            usuarioMongo = jsonObject.get("usuario").getAsString();
+            contrasenaMongo = jsonObject.get("password").getAsString();
+            emailMongo = jsonObject.get("email").getAsString();
+            JsonArray librosJson = jsonObject.getAsJsonArray("libros");
+
+            for (JsonElement libroJson : librosJson) {
+                libroObj = libroJson.getAsJsonObject();
+                titulo = libroObj.get("titulo").getAsString();
+                autor = libroObj.get("autor").getAsString();
+                sinopsis = libroObj.get("sinopsis").getAsString();
+                libros.add(new Libro(titulo,autor,sinopsis));
+            }
+            if(usuarioMongo.equals(nombre) && contrasenaMongo.equals(password)){
+                model.addAttribute("nombre", usuarioMongo);
+                model.addAttribute("password", contrasenaMongo);
+                model.addAttribute("email", emailMongo);
+                model.addAttribute("libros", libros);
+                datos = "usuario";
+            }
+
+
+
+        } catch (MongoException e) {
+            System.err.println("Error al obtener usuario: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.err.println("No existe dicho usuario.");
+        }
+        return datos;
+    }
+
+    @PostMapping("/insertar-libro")
+    public String insertarLibro(@RequestParam("nombre") String nombre,
+                                @RequestParam("password") String password,
+                                Model model,
+                                @RequestParam("titulo") String titulo,
+                                @RequestParam("autor") String autor,
+                                @RequestParam("sinopsis") String sinopsis) {
+
+        Conexion conexion = new Conexion();
+
+        Operaciones.insertarLibro(conexion.getDatabase(), nombre, titulo, autor, sinopsis);
+
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("password", password);
+
+        return procesarFormulario(nombre, password, model);
     }
 
 
